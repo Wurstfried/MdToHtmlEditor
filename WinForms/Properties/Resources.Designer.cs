76dcb8d95d96b3988485d4412909e6d674a36ea4
Sebastian Raffel <sebastian.raffel@outlook.de>
2021-02-07 18:12:11 +0100

    Move Resources to SolutionDir

--- WinForms/Properties/Resources.Designer.cs
+++ WinForms/Properties/Resources.Designer.cs
@@ -59,49 +59,5 @@ namespace TestMonacoForms.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to &lt;!doctype html&gt;
-        ///&lt;html lang=&quot;en&quot;&gt;
-        ///&lt;head&gt;
-        ///    &lt;meta charset=&quot;utf-8&quot;&gt;
-        ///    &lt;title&gt;monaco editor&lt;/title&gt;
-        ///    &lt;link rel=&quot;stylesheet&quot; href=&quot;style.css&quot;&gt;
-        ///    &lt;link rel=&quot;stylesheet&quot; data-name=&quot;vs/editor/editor.main&quot; href=&quot;node_modules/monaco-editor/min/vs/editor/editor.main.css&quot;&gt;
-        ///&lt;/head&gt;
-        ///&lt;body&gt;
-        ///    &lt;div id=&quot;container&quot;&gt;&lt;/div&gt;
-        ///    &lt;script src=&quot;node_modules/monaco-editor/min/vs/loader.js&quot;&gt;&lt;/script&gt;
-        ///
-        ///    &lt;script&gt;
-        ///        // Based on https://jsfiddle.net/developit/bwgkr6uq/ which just works but is based on un [rest of string was truncated]&quot;;.
-        /// </summary>
-        internal static string editor {
-            get {
-                return ResourceManager.GetString("editor", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to &lt;!doctype html&gt;
-        ///&lt;html lang=&quot;en&quot;&gt;
-        ///&lt;head&gt;
-        ///    &lt;meta charset=&quot;utf-8&quot;&gt;
-        ///    &lt;title&gt;monaco editor&lt;/title&gt;
-        ///    &lt;link rel=&quot;stylesheet&quot; href=&quot;style.css&quot;&gt;
-        ///    &lt;link rel=&quot;stylesheet&quot; data-name=&quot;vs/editor/editor.main&quot; href=&quot;node_modules/monaco-editor/min/vs/editor/editor.main.css&quot;&gt;
-        ///&lt;/head&gt;
-        ///&lt;body&gt;
-        ///    &lt;div id=&quot;container&quot;&gt;&lt;/div&gt;
-        ///    &lt;script src=&quot;node_modules/monaco-editor/min/vs/loader.js&quot;&gt;&lt;/script&gt;
-        ///
-        ///    &lt;script&gt;
-        ///        // Based on https://jsfiddle.net/developit/bwgkr6uq/ which just works but is based on un [rest of string was truncated]&quot;;.
-        /// </summary>
-        internal static string viewer {
-            get {
-                return ResourceManager.GetString("viewer", resourceCulture);
-            }
-        }
     }
 }
