997febeaa85b79bc65cf93532e0fc63cb2613962
Sebastian Raffel <sebastian.raffel@outlook.de>
2021-02-07 22:15:24 +0100

    Copy only what is necessary

--- WinForms/Form1.Designer.cs
+++ WinForms/Form1.Designer.cs
@@ -96,7 +96,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1231, 536);
             this.Controls.Add(this.theme);
             this.Controls.Add(this.splitView);
