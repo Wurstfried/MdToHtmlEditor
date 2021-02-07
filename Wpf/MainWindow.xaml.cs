689d97544f27e4367231b24b5738bddc7ff6b32d
Sebastian Raffel <sebastian.raffel@outlook.de>
2021-02-07 23:32:15 +0100

    WPF: Establish onkeyup event

--- Wpf/MainWindow.xaml.cs
+++ Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Web.WebView2.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,51 @@ namespace Wpf
         public MainWindow()
         {
             InitializeComponent();
+            //webView.NavigationStarting += EnsureHttps;
+            InitializeAsync();
+        }
+
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.WebMessageReceived += UpdateHtml;
+
+            
+            
+        }
+
+        void UpdateHtml(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            // Do the timer stuff
+
+
+            String uri = args.TryGetWebMessageAsString();
+            //addressBar.Text = uri;
+            webView.CoreWebView2.PostWebMessageAsString(uri);
+
+            //await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
+        }
+
+        void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            
+
+            string uri = args.Uri;
+            if (!uri.StartsWith("https://"))
+            {
+                args.Cancel = true;
+            }
+        }
+
+        private async void webView_Loaded(object sender, RoutedEventArgs e)
+        {
+            //await webView.CoreWebView2.ExecuteScriptAsync(@"window.document.body.onkeyup = function(){window.chrome.webview.postMessage(window.document.URL)}");
+        }
+
+        private async void webView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            await webView.CoreWebView2.ExecuteScriptAsync(@"window.document.body.onkeyup = function(){window.chrome.webview.postMessage(window.document.URL)}");
+
         }
     }
 }
